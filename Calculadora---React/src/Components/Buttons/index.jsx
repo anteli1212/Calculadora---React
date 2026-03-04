04d6ce65cfc65d4e1eb6d6e922a2f93fc1d84d42
anteli1212 <guilherme.dotelilima@gmail.com>
2026-03-04 20:21:09 -0300

    Delete Calculadora---React directory

--- Calculadora---React/src/Components/Buttons/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Buttons({symbol, action, width, color}){
-    return(
-        <div style={{width: width, height: width}}>
-            <button style={{backgroundColor: color, borderRadius: 20, width: "100%", height:'100%', display:"flex", alignItems:'center', justifyContent:'center'}} onClick={action}>{symbol}</button>
-        </div>
-    )
-}
