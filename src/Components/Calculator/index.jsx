017d3fa3678531116f66b4a3ed6a804a985cfe62
anteli1212 <guilherme.dotelilima@gmail.com>
2026-03-04 20:21:29 -0300

    Segundo Commit

--- src/Components/Calculator/index.jsx
+++ src/Components/Calculator/index.jsx
@@ -6,69 +6,45 @@ import "./calculator.css"
 
 export default function Calculator(){
 
-    const [displayNum, setDisplayNum] = useState('0')
-    const [nums, setNums] = useState([])
-    const [operation, setOperation] = useState('')
-    const [result, setResult] = useState(0);
-
-    function AddNum(n){
-        if(displayNum == '0'){
-            setDisplayNum(n)
-
-            return;
-        }
-
-        setDisplayNum(displayNum + n)
+    const initialStates = {
+        displayValue: '0',
+        operation: null,
+        values: [0, 0],
+        current: 0
     }
 
-    function UpdateOperation(op){
-        setOperation(op)
+    const [states, setStates] = useState(...initialStates)
 
-        if(nums.length < 2){
-            setNums([...nums, parseFloat(displayNum)])
-            return;
-        }
+    function AddNum(n){
+        setStates(prev => {
+            return{...prev, displayValue: prev.displayValue === '0' ? n : prev.displayValue + n}
+        })
+    }
+
+    function SetOperation(op){
+
+        setStates(prev => {
+            return{...prev, displayValue: '0', operation: op, current: prev.current === 0 ? prev.current + 1 : prev.current - 1}
+        })
     }
 
     function GetResult(){
-        
-        switch(operation){
-            case '+':
-                setResult(nums[0] + nums[1]);
-                break;
-
-            case '-':
-                setResult(nums[0] - nums[1])
-                break;
-
-            case '/':
-                setResult(nums[0] / nums[1])
-                break;
-
-            case '*':
-                setResult(nums[0] * nums[1])
-                break;
-        }
-
-        setDisplayNum(result);
 
     }
 
     function ClearDisplay(){
-        setDisplayNum('0')
-        setNums([]);
     }
 
     return(
         <div className="container">
             <div className="TopBox">
-                <p className="NumberDisplay">{displayNum}</p>
+                <p className="NumberDisplay">{states.displayValue}</p>
             </div>
 
             <div className="BottomBox">
                 <div className="ButtonsGroup">
                     <Buttons symbol="AC" action={() => ClearDisplay('0')} width="74%" color="#A7A7A7"/>
-                    <Buttons symbol="/" action={() => UpdateOperation('/')} width="20%" color="#FD9427"/>
+                    <Buttons symbol="/" action={() => SetOperation('/')} width="20%" color="#FD9427"/>
                 </div>
 
                 <br/>
@@ -77,7 +53,7 @@ export default function Calculator(){
                     <Buttons symbol="7" action={() => AddNum('7')} width="20%" color="#333333"/>
                     <Buttons symbol="8" action={() => AddNum('8')} width="20%" color="#333333"/>
                     <Buttons symbol="9" action={() => AddNum('9')} width="20%" color="#333333"/>
-                    <Buttons symbol="*" action={() => UpdateOperation('*')} width="20%" color="#FD9427"/>
+                    <Buttons symbol="*" action={() => SetOperation('*')} width="20%" color="#FD9427"/>
                 </div>
 
                 <br />
@@ -86,7 +62,7 @@ export default function Calculator(){
                     <Buttons symbol="4" action={() => AddNum('4')} width="20%" color="#333333"/>
                     <Buttons symbol="5" action={() => AddNum('5')} width="20%" color="#333333"/>
                     <Buttons symbol="6" action={() => AddNum('6')} width="20%" color="#333333"/>
-                    <Buttons symbol="-" action={() => UpdateOperation('-')} width="20%" color="#FD9427"/>
+                    <Buttons symbol="-" action={() => SetOperation('-')} width="20%" color="#FD9427"/>
                 </div>
 
                 <br />
@@ -95,7 +71,7 @@ export default function Calculator(){
                     <Buttons symbol="1" action={() => AddNum('1')} width="20%" color="#333333"/>
                     <Buttons symbol="2" action={() => AddNum('2')} width="20%" color="#333333"/>
                     <Buttons symbol="3" action={() => AddNum('3')} width="20%" color="#333333"/>
-                    <Buttons symbol="+" action={() => UpdateOperation('+')} width="20%" color="#FD9427"/>
+                    <Buttons symbol="+" action={() => SetOperation('+')} width="20%" color="#FD9427"/>
                 </div>
 
                 <br />
